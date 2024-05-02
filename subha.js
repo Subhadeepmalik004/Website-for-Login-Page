78f9256902126aaabdd3fbd8b30ba6452e59a7cf
Subhadeep Malik <subhadeepmalik004@gmail.com.com>
2024-05-02 09:32:39 +0530

    first commit

---
+++ subha.js
@@ -0,0 +1,35 @@
+
+// String --> inmutable
+// array --> mutable 
+
+// const student = {
+//     fullName: "Subhadeep",
+//     age: 20,
+//     CGPA: 8.2,
+//     ispass: true
+// };
+// alert("Hi Sir");
+// let name = prompt("Your Name");
+// let pass;
+// if(name === "subha"){
+//     pass = "Hello Sir";
+// }
+// else if(name === "subhadeep malik"){
+//     pass = "Hello Sir";
+// }
+// else if(name === "Subha"){
+//     pass = "Hello Sir";
+// }
+// else if(name === "Subhadeep Malik"){
+//     pass = "Hello Sir";
+// }else{
+//     pass = "Sorry!"
+// }
+// console.log(pass);
+// alert("Hi Sir");
+// let fullname = prompt("Your Name");
+// console.log(fullname);
+// let str.length = ;
+// let username = "@" + fullname + fullname.length;
+// console.log(username);
+// console.log("@"+name+name.leng);
